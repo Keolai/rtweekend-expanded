c7eb39830d80ca4e0829fa0463fa03bce07ea7b5
Keolailani Rose <krose102202@gmail.com>
2026-07-02 16:43:43 -0500

    changed light class arch

--- include/light.h
+++ include/light.h
@@ -19,28 +19,36 @@ public:
     virtual const vec3 &get_color() const { return light_color; }
     virtual const float get_radius() const { return radius; }
 
-    virtual color light_hit(const hittable &world, const ray &r, hit_record &rec, int shadow_samples) const
+    color light_hit(const hittable &world, const ray &r, hit_record &rec, int shadow_samples) const
     {
         hit_record shadow_rec;
         color lighting = color(0.);
         for (int j = 0; j < shadow_samples; j++)
         {
-            vec3 jitter = random_in_unit_sphere() * this->get_radius();
-            vec3 to_light = ((this->origin()) + jitter) - rec.p;
-            ray light_direction = ray(rec.p + 0.001 * rec.geometry_normal, unit_vector(to_light));
+            lighting += light_sample(world, r, rec) / shadow_samples;
+        }
+        return lighting;
+    }
 
-            if (!world.hit(light_direction, interval(0.001, to_light.length()), shadow_rec)) // not blocked
-            {
-                double NdotL =
-                    std::max(0.,
-                             dot(rec.geometry_normal,
-                                 light_direction.direction()));
+    virtual color light_sample(const hittable &world, const ray &r, hit_record &rec) const
+    {
+        hit_record shadow_rec;
+        color lighting = color(0.);
+        vec3 jitter = random_in_unit_sphere() * this->get_radius();
+        vec3 to_light = ((this->origin()) + jitter) - rec.p;
+        ray light_direction = ray(rec.p + 0.001 * rec.geometry_normal, unit_vector(to_light));
 
-                lighting +=
-                    rec.mat->get_albedo(rec) * this->get_color() * this->power *
-                    NdotL /
-                    ((to_light.length() * to_light.length()) * shadow_samples);
-            }
+        if (!world.hit(light_direction, interval(0.001, to_light.length()), shadow_rec)) // not blocked
+        {
+            double NdotL =
+                std::max(0.,
+                         dot(rec.geometry_normal,
+                             light_direction.direction()));
+
+            lighting =
+                rec.mat->get_albedo(rec) * this->get_color() * this->power *
+                NdotL /
+                (to_light.length() * to_light.length());
         }
         return lighting;
     }
@@ -75,34 +83,31 @@ public:
         cos_outer = std::cos(outer_cone);
     }
 
-    color light_hit(const hittable &world, const ray &r, hit_record &rec, int shadow_samples) const override
+    color light_sample(const hittable &world, const ray &r, hit_record &rec) const override
     {
         hit_record shadow_rec;
         color lighting = color(0.);
-        for (int j = 0; j < shadow_samples; j++)
+        vec3 jitter = random_in_unit_sphere() * this->get_radius();
+        vec3 to_light = ((this->origin()) + jitter) - rec.p;
+        double distance = to_light.length();
+        ray light_direction = ray(rec.p + 0.001 * rec.geometry_normal, unit_vector(to_light));
+
+        if (!world.hit(light_direction, interval(0.001, distance), shadow_rec)) // not blocked
         {
-            vec3 jitter = random_in_unit_sphere() * this->get_radius();
-            vec3 to_light = ((this->origin()) + jitter) - rec.p;
-            double distance = to_light.length();
-            ray light_direction = ray(rec.p + 0.001 * rec.geometry_normal, unit_vector(to_light));
+            double NdotL =
+                std::max(0.,
+                         dot(rec.geometry_normal,
+                             light_direction.direction()));
 
-            if (!world.hit(light_direction, interval(0.001, distance), shadow_rec)) // not blocked
-            {
-                double NdotL =
-                    std::max(0.,
-                             dot(rec.geometry_normal,
-                                 light_direction.direction()));
+            vec3 wi = to_light / distance;
 
-                vec3 wi = to_light / distance;
+            double true_falloff = (dot(-1 * wi, unit_vector(direction)) - cos_outer) / (cos_inner - cos_outer);
 
-                double true_falloff = (dot(-1 * wi, unit_vector(direction)) - cos_outer) / (cos_inner - cos_outer);
+            double falloff = std::max(0., std::min(1., true_falloff));
 
-                double falloff = std::max(0., std::min(1., true_falloff));
-
-                lighting +=
-                    rec.mat->get_albedo(rec) * this->get_color() * this->power * NdotL * falloff /
-                    ((distance * distance) * shadow_samples);
-            }
+            lighting =
+                rec.mat->get_albedo(rec) * this->get_color() * this->power * NdotL * falloff /
+                ((distance * distance));
         }
 
         return lighting;
