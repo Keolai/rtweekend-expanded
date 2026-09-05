c91ca442847cfe46c5cd478b7ea9ff3cc61f01ec
Keolailani Rose <krose102202@gmail.com>
2026-09-04 23:35:42 -0500

    organization

---
+++ include/physics/objects/force.h
@@ -0,0 +1,17 @@
+#ifndef FORCE_H
+#define FORCE_H
+
+#include "../utilities/vec3.h"
+
+class world_force
+{
+public:
+    int strength = 0;
+    vec3 direction = vec3(0);
+};
+class local_force : public world_force
+{
+    public:
+    vec3 position = vec3(0);
+};
+#endif
