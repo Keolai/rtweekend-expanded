e9b060a744b00b408a5d13bf38065bab3f942b56
Keolai <krose102202@gmail.com>
2026-09-14 17:43:04 -0500

    balls can hit each other!

--- main.cpp
+++ main.cpp
@@ -161,18 +161,33 @@ int main()
     sim.add_force(vec3(0,-1,0),5.0); 
     sim.add_wind_resistance(0.1,1); //wind resistance I guess
 
-    int sphere_physics_id = sim.add_sphere_to_world(vec3(0.27,5,0),0.5,false); //Doesnt like when I add a force and a sphere, but can add two of either
-    auto mySphere = std::make_shared<sphere>(point3(0.27,5,0), 0.5, mat);
-    world.add(mySphere);
+    // int sphere_physics_id = sim.add_sphere_to_world(vec3(0.27,5,0),0.5,false); 
+    // auto mySphere = std::make_shared<sphere>(point3(0.27,5,0), 0.5, mat);
+    // world.add(mySphere);
 
-    // int sphere_physics_id_rigid = sim.add_sphere_to_world(vec3(0,-5,0),4.0,true);
-    // auto mySphere2 = std::make_shared<sphere>(point3(0,-5,0), 4.0, mat);
-    // world.add(mySphere2);
+    // // int sphere_physics_id_rigid = sim.add_sphere_to_world(vec3(0,-5,0),4.0,true);
+    // // auto mySphere2 = std::make_shared<sphere>(point3(0,-5,0), 4.0, mat);
+    // // world.add(mySphere2);
+    // int model = sim.add_mesh_to_world("models/trench_cube.obj",true);
+    // auto my_model = std::make_shared<mesh>("models/trench_cube.obj",mat,world);
 
-    int model = sim.add_mesh_to_world("models/open_cube.obj",true);
-    auto my_model = std::make_shared<mesh>("models/open_cube.obj",tex_mat,world);
+    // sim.connect_objects(mySphere->id,sphere_physics_id);
+    // sim.connect_objects(my_model->id, model);
 
-    sim.connect_objects(mySphere->id,sphere_physics_id);
+    // START OF TWO BALLS HITTING DEMO
+    int model = sim.add_mesh_to_world("models/trench_cube.obj",true);
+    auto my_model = std::make_shared<mesh>("models/trench_cube.obj",tex_mat,world);
+    
+    int sphere_physics_id = sim.add_sphere_to_world(vec3(-1,5,0),0.5,false); 
+    auto mySphere1 = std::make_shared<sphere>(point3(-1,5,0), 0.5, mat);
+    world.add(mySphere1);
+
+    int sphere_physics_id2 = sim.add_sphere_to_world(vec3(1,5,0),0.5,false); 
+    auto mySphere2 = std::make_shared<sphere>(point3(1,5,0), 0.5, mat);
+    world.add(mySphere2);
+
+    sim.connect_objects(mySphere1->id,sphere_physics_id);
+    sim.connect_objects(mySphere2->id,sphere_physics_id2);
     sim.connect_objects(my_model->id, model);
 
     //sim.connect_objects(mySphere2->id,sphere_physics_id_rigid);
